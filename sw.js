f635c19a5fd76b41fba759c16a0f587a4eb98b2a
grupomsoft-dotcom <grupomsoft@gmail.com>
2026-03-21 14:50:53 -0300

    Add fetch event listener to service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const cacheName = 'sgp-v3.8';
 const assets = ['./', './index.html', './script.js'];
-
+self.addEventListener('fetch', () => {});
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
